bf0c07f9a1154ee7854a0bebb41bee4fe6dd899d
Omar Nava <omarnavaalvarez@gmail.com>
2018-03-13 14:40:18 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,4 +15,5 @@ end
 def assign_rooms(attendees)
   welcome_array = []
   attendees.each_with_index { |name, i| welcome_array << "Hello, #{name}! You'll be assigned to room #{i + 1}!" }
+  return welcome_array
 end
