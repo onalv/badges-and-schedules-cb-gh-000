4495cb3c312a8de9ccdd3dfa1b9b954779c8aca4
Omar Nava <omarnavaalvarez@gmail.com>
2018-03-13 14:44:57 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,7 +9,7 @@ def batch_badge_creator(attendees)
 end
 
 def printer(attendees)
-  batch_badge_creator.each{|attendee| puts attendee}
+  batch_badge_creator(attendees).each{|attendee| puts attendee}
 end
 
 def assign_rooms(attendees)
